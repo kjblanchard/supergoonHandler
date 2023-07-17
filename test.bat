3299a2dc613d0df3c9f043f93db6e6cee73b58f3
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-07-16 21:03:54 -0400

    real boi

--- test.bat
+++ test.bat
@@ -1,13 +1,5 @@
 @echo off
-powershell.exe -NoExit -Command "& {cd /d '%~dp0'; .\supergoonHandlerExe.exe}"
-@echo off
-start powershell.exe -NoExit -Command "cd /d '%~dp0'; .\supergoonHandlerExe.exe"
-
-@echo off
-echo Start-Process -FilePath "%~dp0\supergoonHandlerExe.exe" -Verb RunAs | powershell.exe -NoExit -Command -
-
-@echo off
-powershell.exe -NoExit -Command "Start-Process -FilePath '%~dp0\supergoonHandlerExe.exe' -Verb RunAs"
+for %%I in ("%~dp0") do start cmd /k "cd /d %%~fI && supergoonHandlerExe.exe"
 
 
 
