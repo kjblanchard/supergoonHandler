ba85e3f92b2874c538066dff0541cb95d0ea4a8d
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-07-16 20:49:51 -0400

    simple ps

--- test.bat
+++ test.bat
@@ -1,2 +1,5 @@
 @echo off
 powershell.exe -NoExit -Command "& {Start-Process -FilePath './supergoonHandlerExe.exe' -Verb RunAs}"
+
+@echo off
+powershell.exe -NoExit -Command "Set-Location -Path '%~dp0'; Start-Process -FilePath 'supergoonHandlerExe.exe' -Verb RunAs"
