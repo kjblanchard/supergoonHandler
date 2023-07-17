e1bd50a3bd46f891b581f087d7efc756797821c7
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-07-16 21:01:46 -0400

    real boi

--- test.bat
+++ test.bat
@@ -6,4 +6,8 @@ start powershell.exe -NoExit -Command "cd /d '%~dp0'; .\supergoonHandlerExe.exe"
 @echo off
 echo Start-Process -FilePath "%~dp0\supergoonHandlerExe.exe" -Verb RunAs | powershell.exe -NoExit -Command -
 
+@echo off
+powershell.exe -NoExit -Command "Start-Process -FilePath '%~dp0\supergoonHandlerExe.exe' -Verb RunAs"
+
+
 
